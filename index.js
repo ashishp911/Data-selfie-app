9895673c4f346a80034a6689facae214c12dd73b
ashishp911 <ashish.patil35@gmail.com>
2023-01-12 14:36:19 -0500

    created public folder

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ console.log("Here");
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log('Listening at 3000'))
+app.listen(3000, () => console.log('Listening at 3000'));
+
+// using express to host static files
+
+app.use(express.static('public'));
