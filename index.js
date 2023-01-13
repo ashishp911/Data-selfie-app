07ce2ee321b3982076ba8de104012986d08fb1a9
ashishp911 <ashish.patil35@gmail.com>
2023-01-13 15:02:15 -0500

    request recieved from client using the post request

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ const app = express();
 app.listen(3000, () => console.log('Listening at 3000'));
 
 // using express to host static files
-
 app.use(express.static('public'));
+app.use(express.json({ limit : '1mb' }));
+app.post('/api', (req, res) =>{
+    // request received from client 
+    console.log(req.body);
+    // sending response to client
+});
