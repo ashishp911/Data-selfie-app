7095a5f3d4e281d84afab21bbcf02e8979201ce4
ashishp911 <ashish.patil35@gmail.com>
2023-01-13 18:28:16 -0500

    installed nedb

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ app.listen(3000, () => console.log('Listening at 3000'));
 // using express to host static files
 app.use(express.static('public'));
 app.use(express.json({ limit : '1mb' }));
-app.post('/api', (req, res) =>{
+
+app.post('/api', (request, response) =>{
     // request received from client 
-    console.log(req.body);
+    console.log(request.body);
     // sending response to client
+    response.json({
+        status : 'success',
+        latitude : request.body.lat,
+        longitude : request.body.lon,
+    })
 });
