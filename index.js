62458022f1ec34a41689d5ac11fd86a34dd4ae9a
ashishp911 <ashish.patil35@gmail.com>
2023-01-16 10:56:32 -0500

    added name field to add to database

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 // console.log("Here");
 
 const express = require('express');
-const app = express();
+const Datastore = require('nedb');
 
+const app = express();
 app.listen(3000, () => console.log('Listening at 3000'));
 
 // using express to host static files
 app.use(express.static('public'));
 app.use(express.json({ limit : '1mb' }));
 
+// Adding database  
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (request, response) =>{
     // request received from client 
     console.log(request.body);
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+
     // sending response to client
     response.json({
         status : 'success',
-        latitude : request.body.lat,
-        longitude : request.body.lon,
+        timestamp : timestamp,
+        Name : data.username,
+        latitude : data.lat,
+        longitude : data.lon,
     })
 });
